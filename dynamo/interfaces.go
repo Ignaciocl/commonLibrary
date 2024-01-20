ef868db9d08990ef4a4f0edf0a1391cd19333251
ignacio lugones <ignacio.lugones@wibson.org>
2024-01-20 02:47:27 -0300

    fix interface

--- dynamo/interfaces.go
+++ dynamo/interfaces.go
@@ -8,6 +8,6 @@ type Table[T StorableDynamo] interface {
 	Put(item T) error
 	Get(hash string) (T, error)
 	Delete(hash string) (T, error)
-	QueryBy(parameter string, queryBy interface{}, filter string, valuesToFilter []interface{}) ([]T, error)
+	QueryBy(parameter string, queryBy interface{}, indexName, filter string, valuesToFilter []interface{}) ([]T, error)
 	Scan(query string, valuesToFilter []interface{}) ([]T, error)
 }
